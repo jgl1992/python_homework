86cb8528a83e606c714ca23a31c48a17621ca192
Justina <justinalin92@gamil.com>
2026-07-14 23:39:04 -0500

    add

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -1,18 +1,18 @@
 def run_diary():
-    try:
-        with open('diary.txt', 'a') as diary:
-            prompt = "What happened? "
-            while true:
-                try:
-                    line = input(prompt)
-                except EOFError:
-                    raise
-                if line == 'Done':
-                    diary.write(line + '\n')
-                    break
+try:
+with open('diary.txt', 'a') as diary:
+prompt = "What happens? "
+while true:
+try:
+line = input(prompt)
+    except EOFError:
+    raise
+    if line == 'Done':
+diary.write(line + '\n')
+         break
                 diary.write(line + '\n')
-                prompt = "What else"
-    except Exception as e:
+prompt = "What_else"
+except Exception as e:
         trace_back = traceback.extract_tb(e.__traceback__)
         stack_trace = list()
         for trace in trace_back:
@@ -22,42 +22,54 @@ def run_diary():
         if message:
             print(f"Exception message: {message}")
         print(f"Stack trace: {stack_trace}")
+import traceback
+except Exception as e:
+   trace_back = traceback.extract_tb(e.__traceback__)
+   stack_trace = list()
+   for trace in trace_back:
+      stack_trace.append(f'File : {trace[0]} , Line : {trace[1]}, Func.Name : {trace[2]}, Message : {trace[3]}')
+   print(f"Exception type: {type(e).__name__}")
+   message = str(e)
+   if message:
+print(f"Exception message: {message}")
+   print(f"Stack trace: {stack_trace}")
 def read_employees():
-    global employees
+global employees
     rows = []
     d = {}
     try:
-        with open('../csv/employees.csv') as f:
+with open('../csv/employees.csv') as f:
             reader = csv.reader(f)
             for i, row in enumerate(reader):
                 if i == 0:
                     d['fields'] = row
-                else:
-                    rows.append(row)
+else:
+rows.append(row)
     except Exception as e:
         print("Error reading employees.csv: ", e)
         raise
     d['rows'] = rows list(fil
     employees = d
-    return
+return
 def column_index(col_name):
-    return employees["fields"].index("first_name")
+return employees["fields"].index("first_name")
 def first_name(row_number):
-    idx = column_index('first_name')
-    return employees['rows'][row_number][idx]
+idx = column_index('first_name')
+return employees['rows'][row_number][idx]
 def employee_find_2((employee_id):
-    matches = list(filter(lambda row: int(row[employee_id_column]) == employee_id,
+matches = list(filter(lambda row:int(row[employee_id_column]) == employee_id,
 ('employees["rows"]))
-    return matches
+return matches
 def sort_by_last_name():
-    last_name_col = column_index('last_name')
-    return sorted(employees['rows'], key=lambda row: row[last_name_col])
+last_name_col = column_index('last_name')
+return sorted(employees['rows'], key=lambda row: row[last_name_col])
 def employee_dict
     employee_id_col = column_index('employee_id')
     d = {}
     for row in employees['rows']:
     emp_id =int(row[employee_id_col])
     d[em[_id] = row
+matches=list(filter(employee_match, employees["rows"]))
 return d
 def all_employees_dict():
     employee_id_col = column_index('employee_id')
@@ -92,15 +104,16 @@ def read_minutes():
     return minutes1, minutes2
 
 minutes_list = []
-
-#14 Convert strings to datetime
 def create_minutes_set():
     global minutes_set, minutes_list
     combined = minutes1['rows'] + minutes2['rows']
     minutes_list = list(map(lambda x: (x[0], datetime.strptime(x[1], '%B %d, %Y')), combined))
     return minutes_list
+secret = "shazam!"
 
-#15 Write out sorted list
+def set_secret(new_secret):
+   global secret
+   secret = new_secret
 def write_sorted_list():
     sorted_minutes = sorted(minutes_list, key=lambda t: t[1])
     converted = list(map(lambda t: (t[0], t[1].strftime('%B %d, %Y')), sorted_minutes))
